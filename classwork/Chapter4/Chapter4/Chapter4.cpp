8dba789a3322646da0716b182e57d193cdb8b1e6
MICHAEL.TAYLOR <MICHAEL.TAYLOR@INFOTECH-67>
2025-09-23 21:34:28 -0500

    Added more input validation of ranges. Discussed if-else, if-else-if and nested ifs. Discussed logical operators.

--- classwork/Chapter4/Chapter4/Chapter4.cpp
+++ classwork/Chapter4/Chapter4/Chapter4.cpp
@@ -34,26 +34,77 @@ int main()
 
     std::cout << "Enter the release year (1900-2100): ";
     std::cin >> releaseYear;
-    if (releaseYear < 1900)
+    /*if (releaseYear < 1900)
     {
         std::cout << "Release year must be at least 1900" << std::endl;
         releaseYear = 1900;
-    };
+    } else if (releaseYear > 2100)
+    {
+        std::cout << "Release year must be between 1900 and 2100" << std::endl;
+        releaseYear = 1900;
+    }*/
+
+    // Logical AND &&   Eb && Eb => bool
+    // Logical OR ||    Eb || Eb => bool
+    // Logical NOT !    !Eb => bool
+    if (releaseYear < 1900 || releaseYear > 2100)
+    {
+        std::cout << "Release year must be between 1900 and 2100" << std::endl;
+        releaseYear = 1900;
+    }
 
     std::cout << "Enter the optional description: ";
     std::getline(std::cin, description);
 
-    //TODO: Validate userRating
+    // Validate userRating
     std::cout << "Enter the optional user rating (1.0-10.0): ";
     std::cin >> userRating;
+    if (userRating < 1.0 || userRating > 10.0)
+    {
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 1.0;
+    }
+    /*if (userRating < 1.0)
+    {
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 1.0;
+    } else if (userRating > 10.0)
+    {
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 1.0;
+    }*/
 
     std::cout << "Is this a classic (Y/N)? ";
     std::string input;
     std::cin >> input;
-    if (_strcmpi(input.c_str(), "Y") == 0)
+
+    // if (Eb) S [ else S ] ;
+    // Else associates with nearest preceding if
+    // Nested if
+    //   if (E1) 
+    //     if (E2) 
+    //       if (E3)
+    //          S (E1, E2 and E3 must be true)
+    // if-else-if
+    //   if (E1)
+    //     S
+    //   else if (E2)
+    //     S  (E1 is false and E2 is true)
+    // 
+
+    /*if (_strcmpi(input.c_str(), "Y") == 0)
         isClassic = true;
     if (_strcmpi(input.c_str(), "N") == 0)
         isClassic = false;
+    else
+        std::cout << "You must enter either Y or N";*/
+    if (_strcmpi(input.c_str(), "Y") == 0)
+        isClassic = true;
+    else 
+        if (_strcmpi(input.c_str(), "N") == 0)
+            isClassic = false;
+        else
+            std::cout << "You must enter either Y or N";
     //TODO: Handle other values
 }
 
